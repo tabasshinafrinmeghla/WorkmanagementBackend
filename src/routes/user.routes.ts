5be383e82fe5423e94a0a86e7e3076a9c6f7a6f2
tabasshinafrinmeghla <tabasshinafrinmeghla@gmail.com>
2026-06-14 16:55:03 +0600

    add all fixing

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,11 +2,11 @@ import express from "express";
 
 import {
   adminDashboard,
+  hrDashboard,
   employeeDashboard,
 } from "../controllers/user.controller.js";
 
 import { protect } from "../middleware/auth.middleware.js";
-
 import { authorize } from "../middleware/role.middleware.js";
 
 const router = express.Router();
@@ -19,9 +19,16 @@ router.get(
 );
 
 router.get(
-  "/employee",
+  "/hr",
   protect,
-  authorize("employee", "admin"),
+  authorize("hr", "admin"),
+  hrDashboard
+);
+
+router.get(
+  "/employees",
+  protect,
+  authorize("employee", "hr", "admin"),
   employeeDashboard
 );
 
